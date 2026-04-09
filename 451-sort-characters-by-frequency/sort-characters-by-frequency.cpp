450f7a0cf6a1cfa073f94a96d458c6e397af0494
dhanishkasharma <143902844+dhanishkasharma@users.noreply.github.com>
2026-04-09 16:54:13 +0530

    Time: 8 ms (12.93%) | Memory: 17.2 MB (8.15%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-
-   static bool comp(pair<char,int> vp1,pair<char,int> vp2){
-       return vp1.second>vp2.second;
-   }
     string frequencySort(string s) {
         unordered_map<char,int> mpp;
+        int n=s.size();
 
         for(int i=0;i<s.size();i++){
             mpp[s[i]]++;
         }
-           vector<pair<char,int>> vp;
-        for(auto it:mpp){
-            vp.push_back({it.first,it.second});
+          vector<vector<char>> bucket(n+1);
 
-        }
+          for(auto p:mpp){
+            bucket[p.second].push_back(p.first);
+          }
 
-        sort(vp.begin(),vp.end(),comp);
-    string ans="";
-    for(auto p:vp){
-        ans+=string(p.second,p.first);
-    }
+          string ans="";
+
+          for(int i=n;i>=1;i--){
+            for(char ch:bucket[i]){
+                ans+=string(i,ch);
+            }
+          }
 return ans;
 
     }
