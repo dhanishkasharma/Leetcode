e691fdbb4cfc3d580a8735a3e1b9939591ff5023
dhanishkasharma <143902844+dhanishkasharma@users.noreply.github.com>
2026-04-12 01:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (18.88%) - LeetSync

---
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkString(string s) {
+
+        for(int i=1;i<s.size();i++){
+            if(s[i]=='a' && s[i-1]=='b'){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
