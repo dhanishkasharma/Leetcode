8652f7b51f077187f8d8c44d25c9ff16a7684c09
dhanishkasharma <143902844+dhanishkasharma@users.noreply.github.com>
2026-06-03 16:03:51 +0530

    Time: 7 ms (41.12%) | Memory: 16 MB (14.74%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -2,20 +2,88 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n=nums.size();
-   
-     long long sum=0;
+
+
+        vector<int> pse(n,-1);
+        vector<int> nse(n,n);
+
+        vector<int> ple(n,-1);
+        vector<int> lle(n,n);
+
+        stack<int> st1;
+        stack<int> st2;
+
+        stack<int> st;
+        stack<int> st3;
+
         for(int i=0;i<n;i++){
-            int mini=INT_MAX;
-              int maxi=INT_MIN;
-            for(int j=i;j<n;j++){
-                mini=min(mini,nums[j]);
-                maxi=max(maxi,nums[j]);
-
-                sum+=maxi-mini;
-
+            while(!st.empty() && nums[st.top()]>=nums[i]){
+                st.pop();
             }
+
+            if(!st.empty()) pse[i]=st.top();
+
+            st.push(i);
         }
-        return sum;
-        
+
+
+        for(int i=n-1;i>=0;i--){
+            while(!st3.empty() && nums[st3.top()]>nums[i]){
+                st3.pop();
+            }
+
+            if(!st3.empty()) nse[i]=st3.top();
+
+            st3.push(i);
+        }
+
+
+
+    for(int i=0;i<n;i++){
+            while(!st1.empty() && nums[st1.top()]<=nums[i]){
+                st1.pop();
+            }
+
+            if(!st1.empty()) ple[i]=st1.top();
+
+            st1.push(i);
+        }
+
+
+        for(int i=n-1;i>=0;i--){
+            while(!st2.empty() && nums[st2.top()]<nums[i]){
+                st2.pop();
+            }
+
+            if(!st2.empty()) lle[i]=st2.top();
+
+            st2.push(i);
+        }
+
+
+
+
+
+
+
+
+        long long mini=0;
+        for(int i=0;i<n;i++){
+            long long left=i-pse[i];
+            long long right=nse[i]-i;
+            mini+=1LL*nums[i]*left*right;
+
+
+        }
+
+        long long maxi=0;
+        for(int i=0;i<n;i++){
+            long long left=i-ple[i];
+            long long right=lle[i]-i;
+            maxi+=1LL*nums[i]*left*right;
+        }
+
+
+        return maxi-mini;
     }
 };
