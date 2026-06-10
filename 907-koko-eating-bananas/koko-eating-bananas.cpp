e2ee531278fd8c9e4dabe0aeb0cde5f519831103
dhanishkasharma <143902844+dhanishkasharma@users.noreply.github.com>
2026-06-10 15:20:19 +0530

    Time: 8 ms (54.76%) | Memory: 22.9 MB (77.97%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int maxi(vector<int>& piles) {
-        int a = INT_MIN;
-        for (int i = 0; i < piles.size(); i++) {
-            a = max(a, piles[i]);
-        }
-        return a;
-    }
-
-    long long tota(vector<int>& piles, int mid) {
-        long long sum = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            sum += (piles[i] + mid - 1) / mid; // integer ceil
-        }
-        return sum;
-    }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = maxi(piles);
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int ans=INT_MAX;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            long long t = tota(piles, mid);
 
-            if (t <= (long long)h) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            long long sum=0;
+
+            for(int i=0;i<piles.size();i++){
+                sum+=ceil((double)piles[i]/(double)mid);
             }
+
+
+            if(sum<=h){
+                high=mid-1;
+            }
+
+            else{
+                low=mid+1;
+            }
+
+
         }
+
+
         return low;
     }
 };
